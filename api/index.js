90a1f2b69704eda415c5e48a58803a48acfb0e71
zubair shaikh <zubairs559@gmail.com>
2023-12-04 22:32:19 +0530

    Create user update api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 
@@ -16,6 +17,7 @@ catch((err) => {
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log("server is up and running");
