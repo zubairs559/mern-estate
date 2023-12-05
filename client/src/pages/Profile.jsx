00e290523bfe8161bf7861c815a56272a7063edf
zubair shaikh <zubairs559@gmail.com>
2023-12-05 20:17:19 +0530

    Created SignOut Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useRef, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -86,7 +86,22 @@ export default function Profile() {
             } catch (error) {
                 dispatch(deleteUserFailure(error.message))
             }
-        }
+        };
+
+        const handleSignOut = async () =>{
+            try {
+                dispatch(signOutUserStart());
+                const res = await fetch('/api/auth/signout');
+                const data = await res.json();
+                if(data.success === false){
+                    dispatch(deleteUserFailure(data.message));
+                    return;
+                }
+                dispatch(deleteUserSuccess(data));
+            } catch (error) {
+                dispatch(deleteUserFailure(data.message));
+            }
+        };
   return (
     <div className="p-3 max-w-lg mx-auto">
     <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -127,7 +142,7 @@ export default function Profile() {
 
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5 ">{error ? error : ''}</p>
       <p className="text-green-700 mt-5 ">{updateSuccess ? 'User is updated Successfullyy' : ''}</p>
