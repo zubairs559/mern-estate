023f22e08691518dd210a6abab6dc99d3d2535dd
zubair shaikh <zubairs559@gmail.com>
2023-12-14 22:31:44 +0530

    Update Listing Working but 404 error not working

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,5 @@
 import Listing from "../models/listing.model.js";
-import { errorHandler } from '../utils/error.js';
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -29,3 +29,27 @@ export const deleteListing = async (req, res, next) => {
       next(error);
     }
 };
+
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing) {
+      return next(errorHandler(404, 'Listing Not Found'));
+    }
+    
+    if (req.user.id !== listing.userRef) {
+      return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+
+    try {
+      const updatedListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.status(200).json(updatedListing);
+    } catch (error) {
+      next(error);
+    }
+  };
