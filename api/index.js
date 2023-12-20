a393f7c4420817d9e5b3162f027ba8da9bc65005
zubair shaikh <zubairs559@gmail.com>
2023-12-20 23:45:00 +0530

    Add Pre Installation

--- api/index.js
+++ api/index.js
@@ -15,6 +15,8 @@ catch((err) => {
     console.log(err);
 });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -28,6 +30,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
